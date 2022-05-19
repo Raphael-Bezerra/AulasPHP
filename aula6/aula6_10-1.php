ced663979a7ae58b0594f36151c262de871006f9
Raphael-Bezerra <raphabzerra15@gmail.com>
2022-05-18 21:37:43 -0300

    array..count

--- aula6/aula6_10-1.php
+++ aula6/aula6_10-1.php
@@ -3,8 +3,8 @@ $nomes=array("Fulano","Beltrano","Sicrano","Astrogildo");
 $total=count($nomes);
 $ult=count($nomes)-1;
 echo"Foram encontradas $total nomes no array.<br>";
-echo"Primeiro valor do array:".$nomes[0]."<br>";
-echo"Último valor do array:".$nomes[$ult];
+echo"Primeiro valor do array: ".$nomes[0]."<br>";
+echo"Último valor do array: ".$nomes[$ult];
 echo"<hr>";
 for($i=0;$i<$total;$i++){
  echo$nomes[$i]."<br>";
